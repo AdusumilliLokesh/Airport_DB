7da8a36916dc0358f8426b652ca9d7d7642e67e0
LOKESH DHANANJAYA RAO ADUSUMILLI <8489tjc18@gmail.com>
2023-07-29 17:23:26 -0400

    formatting code

--- AirportProject/Frontend/architecture-app/src/App.js
+++ AirportProject/Frontend/architecture-app/src/App.js
@@ -1,32 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+
 import { VerticalTab, VerticalTabs } from './VerticalTabs';
+import { PlaneDetails } from './PlaneDetails';
 function App() {
 
-  const [planes, setPlanes] = useState([]);
-  const itemsPerPage = 5; // Set the number of items to show per page
-  const [currentPage, setCurrentPage] = useState(1);
-
-  // Calculate the total number of pages
-  const totalPages = Math.ceil(planes.length / itemsPerPage);
-
-  // Get the current items to display based on the current page number
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = planes.slice(indexOfFirstItem, indexOfLastItem);
-
-  // Handle changing the page
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  useEffect(() => {
-    // Fetch data from the backend when the component mounts
-    axios.get('/architecture') // The proxy is set to 'http://localhost:5000' in package.json
-      .then(response => setPlanes(response.data))
-      .catch(error => console.error('Error fetching data', error));
-  }, []);
-
+  
   return (
     <div>
 
@@ -47,40 +25,7 @@ function App() {
           </VerticalTab>
           <VerticalTab label="Plane Details">
             <div>
-              <div>
-                <h1>Planes Data</h1>
-                <table style={{ borderCollapse: 'collapse', width: 'fit-content' }}>
-                  <thead>
-                    <tr>
-                      <th style={tableHeaderStyle}>Model</th>
-                      <th style={tableHeaderStyle}>Capacity</th>
-                      <th style={tableHeaderStyle}>Weight</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {currentItems.map((plane, index) => (
-                      <tr key={index}>
-                        <td style={tableCellStyle}>{plane.Model}</td>
-                        <td style={tableCellStyle}>{plane.Capacity}</td>
-                        <td style={tableCellStyle}>{plane.Weight}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-
-              </div>
-
-              <div>
-                {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
-                  <button
-                    key={pageNumber}
-                    onClick={() => handlePageChange(pageNumber)}
-                    disabled={pageNumber === currentPage}
-                  >
-                    {pageNumber}
-                  </button>
-                ))}
-              </div>
+             <PlaneDetails></PlaneDetails>
             </div>
           </VerticalTab>
           <VerticalTab label="Owners">
@@ -101,17 +46,5 @@ function App() {
 
   );
 }
-// Styles for table header cells
-const tableHeaderStyle = {
-  border: '1px solid black',
-  padding: '8px',
-  textAlign: 'left',
-  fontWeight: 'bold',
-};
 
-// Styles for table cells
-const tableCellStyle = {
-  border: '1px solid black',
-  padding: '8px',
-};
 export default App;
