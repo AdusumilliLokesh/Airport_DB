b9e73fca1ae8df5bc4714625bf7c2ceb1740ca92
revanthkoganti <revanthkoganti@gmail.com>
2023-07-29 17:20:39 -0400

    ServerCommit

--- AirportProject/Backend/server.js
+++ AirportProject/Backend/server.js
@@ -8,22 +8,77 @@ const port = 5000;
 const dbConfig = {
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "root",
     port: 3306,
     database: "airport_db"
 };
 
-// Create a connection pool to the MySQL database
 const pool = mysql.createPool(dbConfig);
 
-// Enable CORS for all origins (*)
 app.use(cors());
 
-// Define a route to get architecture data
-app.get('/architecture', async (req, res) => {
+app.get('/getTypeOfPlane', async (req, res) => {
   try {
     const connection = await pool.getConnection();
     const [rows] = await connection.query('Select * from type_of_plane');
+    connection.release();
+    res.json(rows);
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.get('/getAirportAprons', async (req, res) => { 
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT * FROM airport_apron;');
+    connection.release();
+    res.json(rows);
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.get('/getAirportAprons', async (req, res) => { 
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT * FROM airport_apron;');
+    connection.release();
+    res.json(rows);
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.get('/getAirplane', async (req, res) => { 
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT * FROM airplane;');
+    connection.release();
+    res.json(rows);
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.get('/getOwner', async (req, res) => { 
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT * FROM owns;');
+    connection.release();
+    res.json(rows);
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/getEmployee', async (req, res) => { 
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT * FROM employee;');
     connection.release();
     res.json(rows);
   } catch (err) {
