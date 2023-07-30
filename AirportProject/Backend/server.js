461f917b85f35059e91dcc83c2f1828178b3ca6c
revanthkoganti <revanthkoganti@gmail.com>
2023-07-29 20:07:45 -0400

    Search api commit

--- AirportProject/Backend/server.js
+++ AirportProject/Backend/server.js
@@ -16,6 +16,7 @@ const dbConfig = {
 const pool = mysql.createPool(dbConfig);
 
 app.use(cors());
+app.use(express.json()); 
 
 app.get('/getTypeOfPlane', async (req, res) => {
   try {
@@ -76,6 +77,74 @@ app.get('/getEmployee', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+app.post('/searchAirplaneByRegistration', async (req, res) => {
+  try {
+    const { registration_no } = req.body;
+
+    if (!registration_no) {
+      return res.status(400).json({ error: 'Registration number is required' });
+    }
+
+    const regNumber = Number(registration_no);
+
+    const connection = await pool.getConnection();
+
+    const [airplaneRows] = await connection.query('SELECT * FROM airplane WHERE registration_no = ?;', [regNumber]);
+
+    const [ownsRows] = await connection.query('SELECT * FROM owns WHERE registration_no = ?;', [regNumber]);
+
+    if (airplaneRows.length === 0) {
+      connection.release();
+      return res.status(404).json({ error: 'Airplane with the provided registration number not found' });
+    }
+
+    const airplaneModel = airplaneRows[0].Model;
+
+    const [typeOfPlaneRows] = await connection.query('SELECT * FROM type_of_plane WHERE model = ?;', [airplaneModel]);
+
+    const [worksOnRows] = await connection.query('SELECT * FROM works_on WHERE registration_no = ?;', [regNumber]);
+
+    const essnArray = worksOnRows.map((row) => row.Essn);
+
+    if (essnArray.length === 0) {
+      connection.release();
+      return res.json({
+        airplane: airplaneRows[0],
+        owns: ownsRows || [],
+        typeOfPlane: typeOfPlaneRows || [],
+        employees: [],
+        airportApron: [] // Return an empty array since there is no AP_number in airplane data
+      });
+    }
+
+    const [employeeRows] = await connection.query('SELECT * FROM employee WHERE essn IN (?);', [essnArray]);
+
+    // Get the AP_number from the airplane data
+    const apNumber = airplaneRows[0].AP_Number;
+
+    // Query to fetch data from airport_apron based on the AP_number
+    const [airportApronRows] = await connection.query('SELECT * FROM airport_apron WHERE AP_number = ?;', [apNumber]);
+
+    connection.release();
+
+    const response = {
+      airplane: airplaneRows[0],
+      owns: ownsRows || [],
+      typeOfPlane: typeOfPlaneRows || [],
+      employees: employeeRows,
+      airportApron: airportApronRows || []
+    };
+
+    res.json(response);
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+  
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
