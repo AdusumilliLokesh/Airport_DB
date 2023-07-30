0a7e36c98b85ec4bfbc8499d3eb9b26bf579a07f
LOKESH DHANANJAYA RAO ADUSUMILLI <8489tjc18@gmail.com>
2023-07-30 18:12:38 -0400

    forms mapping

--- AirportProject/Frontend/architecture-app/src/OwnerDetails.js
+++ AirportProject/Frontend/architecture-app/src/OwnerDetails.js
@@ -2,13 +2,14 @@
 import './OwnerDetails.css'; // Import the CSS file for styling
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { RiEdit2Line, RiDeleteBinLine, RiAddLine } from 'react-icons/ri';
 
 const OwnerDetails = ({ children }) => {
     const [owners, setowner] = useState([]);
     const itemsPerPage = 10; // Set the number of items to show per page
     const [currentPage, setCurrentPage] = useState(1);
-
+    const [Edit, setEdit] = useState(false);
+    const [Add, setAdd] = useState(false);
     // Calculate the total number of pages
     const totalPages = Math.ceil(owners.length / itemsPerPage);
 
@@ -21,6 +22,58 @@ const OwnerDetails = ({ children }) => {
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
+    const [owner_id, setowner_id] = useState();
+    const[Registration_number,setRegistration_number]=useState();
+    const[Purchase_date,setPurchase_date]=useState();
+    const handleEdit = (id) => {
+        // Implement your edit logic here
+        console.log('Edit item with ID:', id);
+        setEdit(true);
+        setowner_id(id.owner_id);
+        setRegistration_number(id.Registration_number);
+        setPurchase_date(id.Purchase_date);
+        setAdd(false);
+    };
+
+    const handleDelete = (id) => {
+        // Implement your delete logic here
+        console.log('Delete item with ID:', id);
+
+    };
+    const handleAdd = (id) => {
+
+        // Implement your add logic here
+        setAdd(true);
+        setEdit(false);
+        console.log('Add item with ID:', id);
+    };
+    
+  const handleowner_id = (event) => {
+    setowner_id(event.target.value);
+  };
+  const handleRegistration_number = (event) => {
+    setRegistration_number(event.target.value);
+  };
+  const handlePurchase_date = (event) => {
+    setPurchase_date(event.target.value);
+  };
+  const handleowner_idAdd = (event) => {
+    setowner_id(event.target.value);
+  };
+  const handleRegistration_numberAdd = (event) => {
+    setRegistration_number(event.target.value);
+  };
+  const handlePurchase_dateAdd = (event) => {
+    setPurchase_date(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    //setcapacity(event.target.value);
+    
+  };
+  const handleSubmitAdd = (event) => {
+    //setcapacity(event.target.value);
+    
+  };
 
     useEffect(() => {
         // Fetch data from the backend when the component mounts
@@ -32,15 +85,21 @@ const OwnerDetails = ({ children }) => {
 
     return (
         <div>
+            <div className='align_table'>
             <div>
-                <h1>Owners Data</h1>
+                <h1>Owners Data
+                <RiAddLine
+                            onClick={() => handleAdd(123)}
+                            style={{ cursor: 'pointer', marginRight: '10px' }}
+                        />
+                </h1>
                 <table className="ownerAlign">
                     <thead>
                         <tr>
                             <th className="ownerheader">Owner ID</th>
                             <th className="ownerheader">Registration Number</th>
                             <th className="ownerheader">Purchase Date</th>
-                            
+                            <th className="ownerheader">Actions</th>
                            
                         </tr>
                     </thead>
@@ -50,7 +109,20 @@ const OwnerDetails = ({ children }) => {
                                 <td className="ownercell">{owner.owner_id}</td>
                                 <td className="ownercell">{owner.Registration_number}</td>
                                 <td className="ownercell">{owner.Purchase_date}</td>
-                                
+                                <td className="ownercell">
+                                        <span>
+                                            <RiEdit2Line
+                                                onClick={() => handleEdit(owner)}
+                                                style={{ cursor: 'pointer', marginRight: '10px' }}
+                                            />
+                                        </span>
+                                        <span>
+                                            <RiDeleteBinLine
+                                                onClick={() => handleDelete(owner)}
+                                                style={{ cursor: 'pointer' }}
+                                            />
+                                        </span>
+                                    </td>
                             </tr>
                         ))}
                     </tbody>
@@ -69,6 +141,58 @@ const OwnerDetails = ({ children }) => {
                     </button>
                 ))}
             </div>
+            </div>
+            <div className='align_form'>
+                    {Edit ? (
+                        <form onSubmit={handleSubmit}>
+                        <div>Edit Owner
+                        <label>
+                        Owner ID:
+                          <input type="text" value={owner_id} onChange={handleowner_id} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                        Registration_number:
+                          <input type="text" value={Registration_number} onChange={handleRegistration_number} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Purchase_date:
+                          <input type="text" value={Purchase_date} onChange={handlePurchase_date} />
+                        </label>
+                        </div>
+                        <div><button type="submit">Save</button></div>
+                      </form>
+                    ) : null}
+
+                    {Add ?
+                        <form onSubmit={handleSubmitAdd}>
+                        <div>Add Owner:
+                        <label>
+                        Owner_ID:
+                          <input type="text" value={owner_id} onChange={handleowner_idAdd} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                        Registration_number:
+                          <input type="text" value={Registration_number} onChange={handleRegistration_numberAdd} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Purchase_date:
+                          <input type="text" value={Purchase_date} onChange={handlePurchase_dateAdd} />
+                        </label>
+                        </div>
+                        <div><button type="submit">Save</button></div>
+                      </form>
+                        : null}
+                </div>
         </div>
     );
 };
--- AirportProject/Frontend/architecture-app/src/PlaneDetails.js
+++ AirportProject/Frontend/architecture-app/src/PlaneDetails.js
@@ -2,12 +2,14 @@
 import './PlaneDetails.css'; // Import the CSS file for styling
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { RiEdit2Line, RiDeleteBinLine, RiAddLine } from 'react-icons/ri';
 
 const PlaneDetails = ({ children }) => {
     const [planes, setPlanes] = useState([]);
     const itemsPerPage = 10; // Set the number of items to show per page
     const [currentPage, setCurrentPage] = useState(1);
+    const [Edit, setEdit] = useState(false);
+    const [Add, setAdd] = useState(false);
 
     // Calculate the total number of pages
     const totalPages = Math.ceil(planes.length / itemsPerPage);
@@ -16,7 +18,73 @@ const PlaneDetails = ({ children }) => {
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = planes.slice(indexOfFirstItem, indexOfLastItem);
+    const [Model, setModel] = useState();
+    const[Fuel_Capacity,setFuel_Capacity]=useState();
+    const[Weight,setWeight]=useState();
+    const[Maximum_Range	,setMaximum_Range	]=useState();
+    const[Seating_Capacity,setSeating_Capacity]=useState();
+    const handleEdit = (id) => {
+        // Implement your edit logic here
+        console.log('Edit item with ID:', id);
+        setEdit(true);
+        setAdd(false);
+        setModel(id.Model);
+        setWeight(id.Weight);
+        setFuel_Capacity(id.Fuel_Capacity);
+        setMaximum_Range(id.Maximum_Range);
+        setSeating_Capacity(id.Seating_Capacity);
+    };
 
+    const handleDelete = (id) => {
+        // Implement your delete logic here
+        console.log('Delete item with ID:', id);
+
+    };
+    const handleAdd = (id) => {
+
+        // Implement your add logic here
+        setAdd(true);
+        setEdit(false);
+        console.log('Add item with ID:', id);
+    };
+    const handleModel = (event) => {
+        setModel(event.target.value);
+      };
+      const handleFuel_Capacity = (event) => {
+        setFuel_Capacity(event.target.value);
+      };
+      const handleWeight = (event) => {
+        setWeight(event.target.value);
+      };
+      const handleMaximum_Range = (event) => {
+        setMaximum_Range(event.target.value);
+      };
+      const handleSeating_Capacity = (event) => {
+        setSeating_Capacity(event.target.value);
+      };
+      const handleModelAdd = (event) => {
+        setModel(event.target.value);
+      };
+      const handleWeightAdd = (event) => {
+        setWeight(event.target.value);
+      };
+      const handleFuel_CapacityAdd = (event) => {
+        setFuel_Capacity(event.target.value);
+      };
+      const handleMaximum_RangeAdd = (event) => {
+        setMaximum_Range(event.target.value);
+      };
+      const handleSeating_CapacityAdd = (event) => {
+        setSeating_Capacity(event.target.value);
+      };
+      const handleSubmit = (event) => {
+        //setcapacity(event.target.value);
+        
+      };
+      const handleSubmitAdd = (event) => {
+        //setcapacity(event.target.value);
+        
+      };
     // Handle changing the page
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
@@ -32,8 +100,15 @@ const PlaneDetails = ({ children }) => {
 
     return (
         <div>
+            <div className='align_table'>
             <div>
-                <h1>Planes Data</h1>
+                <h1>Planes Data
+                <RiAddLine
+                            onClick={() => handleAdd(123)}
+                            style={{ cursor: 'pointer', marginRight: '10px' }}
+                        />
+
+                </h1>
                 <table className="planeAlign">
                     <thead>
                         <tr>
@@ -42,6 +117,7 @@ const PlaneDetails = ({ children }) => {
                             <th className="planeheader">Weight</th>
                             <th className="planeheader">Maximum_Range</th>
                             <th className="planeheader">Seating_Capacity</th>
+                            <th className="planeheader">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -52,6 +128,20 @@ const PlaneDetails = ({ children }) => {
                                 <td className="planecell">{plane.Weight}</td>
                                 <td className="planecell">{plane.Maximum_Range}</td>
                                 <td className="planecell">{plane.Seating_Capacity}</td>
+                                <td className="aproncell">
+                                        <span>
+                                            <RiEdit2Line
+                                                onClick={() => handleEdit(plane)}
+                                                style={{ cursor: 'pointer', marginRight: '10px' }}
+                                            />
+                                        </span>
+                                        <span>
+                                            <RiDeleteBinLine
+                                                onClick={() => handleDelete(plane)}
+                                                style={{ cursor: 'pointer' }}
+                                            />
+                                        </span>
+                                    </td>
                             </tr>
                         ))}
                     </tbody>
@@ -70,6 +160,86 @@ const PlaneDetails = ({ children }) => {
                     </button>
                 ))}
             </div>
+            </div>
+            <div className='align_form'>
+                    {Edit ? (
+                        <form onSubmit={handleSubmit}>
+                        <div>Edit Plane
+                        <label>
+                        Model:
+                          <input type="text" value={Model} onChange={handleModel} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                        Fuel_Capacity:
+                          <input type="text" value={Fuel_Capacity} onChange={handleFuel_Capacity} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Weight:
+                          <input type="text" value={Weight} onChange={handleWeight} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Maximum_Range:
+                          <input type="text" value={Maximum_Range} onChange={handleMaximum_Range} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Seating_Capacity:
+                          <input type="text" value={Seating_Capacity} onChange={handleSeating_Capacity} />
+                        </label>
+                        </div>
+                        <div><button type="submit">Save</button></div>
+                      </form>
+                    ) : null}
+
+                    {Add ?
+                        <form onSubmit={handleSubmitAdd}>
+                        <div>Add new Plane:
+                        <label>
+                        Model:
+                          <input type="text" value={Model} onChange={handleModelAdd} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                        Fuel_Capacity:
+                          <input type="text" value={Fuel_Capacity} onChange={handleFuel_CapacityAdd} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Weight:
+                          <input type="text" value={Weight} onChange={handleWeightAdd} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Maximum_Range:
+                          <input type="text" value={Maximum_Range} onChange={handleMaximum_RangeAdd} />
+                        </label>
+                        </div>
+                        <div>
+                        <label>
+                            
+                        Seating_Capacity:
+                          <input type="text" value={Seating_Capacity} onChange={handleSeating_CapacityAdd} />
+                        </label>
+                        </div>
+                        <div><button type="submit">Save</button></div>
+                      </form>
+                        : null}
+                </div>
         </div>
     );
 };
